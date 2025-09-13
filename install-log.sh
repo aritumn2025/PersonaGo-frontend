8fba952a664ad71e87782a383cddf4739f1e81fb
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-09-13 21:51:20 +0900

    install and init shadcn

--- install-log.sh
+++ install-log.sh
@@ -1,3 +1,4 @@
 npm install --save-dev prettier prettier-plugin-tailwindcss
 npm install --save-dev @trivago/prettier-plugin-sort-imports
 npm install --save-dev jest @types/jest
+npx shadcn@latest init
