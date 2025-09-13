d4c3fd4ad8854217d89ec08b21bf1dc48a9c7ceb
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-09-13 22:39:47 +0900

    install react-icons

--- install-log.sh
+++ install-log.sh
@@ -2,3 +2,4 @@ npm install --save-dev prettier prettier-plugin-tailwindcss
 npm install --save-dev @trivago/prettier-plugin-sort-imports
 npm install --save-dev jest @types/jest
 npx shadcn@latest init
+npm install react-icons
