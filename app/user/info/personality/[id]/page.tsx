394a0c0dcc89a5235d8e8880c41c6f7feeed2df9
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-10-24 09:48:57 +0900

    性格記事ページの実装

---
+++ app/user/info/personality/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { notFound } from "next/navigation";
+
+import type { PersonalityCode } from "@/constants/personality";
+import { PERSONALITY_ARTICLE_INFO_MAP } from "@/constants/personality_article";
+
+import { BottomNavigation } from "@/components/user/BottomNavigation";
+import { PersonalityArticle } from "@/components/user/info/PersonalityArticle";
+
+interface PageProps {
+  params: { id: PersonalityCode } | Promise<{ id: PersonalityCode }>;
+}
+
+export default async function Page({ params }: PageProps) {
+  const resolvedParams = await Promise.resolve(params); // 一応旧バージョンの互換性を保つために Promise を解決
+  const { id } = resolvedParams;
+
+  if (!(id in PERSONALITY_ARTICLE_INFO_MAP)) {
+    notFound();
+  }
+
+  return (
+    <>
+      <PersonalityArticle personalityCode={id} />
+      <BottomNavigation currentPage="info" />
+    </>
+  );
+}
