8faf59cd4cf7276374d5a35da75f57b3e45c5cb0
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-10-25 00:20:28 +0900

    リダイレクト用ページの実装

---
+++ app/user/start/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect } from "react";
+
+import { useRouter } from "next/navigation";
+
+import { useCookie } from "@/hooks/useCookie";
+
+import { SquareSpin2 } from "@/components/common/loader";
+
+export default function Page() {
+  const { getUserId } = useCookie();
+  const router = useRouter();
+
+  // ユーザーIDの取得とリダイレクト
+  useEffect(() => {
+    const id = getUserId();
+    if (id) {
+      router.replace(`/user/dashboard`);
+    } else {
+      router.replace("/user/diagnose/question");
+    }
+  }, [router]);
+
+  return (
+    <main>
+      <div className="flex h-screen w-full items-center justify-center">
+        <SquareSpin2 color="var(--color-gray-600)" size="36px" />
+      </div>
+    </main>
+  );
+}
