42d3f5f762a9fa4229ff004348f142d124bcf47b
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-10-27 10:32:48 +0900

    ページの適用

--- components/user/info/index.ts
+++ components/user/info/index.ts
@@ -1,4 +1,5 @@
+import { PersonalityListContainer } from "./list/PersonalityListContainer";
 import { PersonalityArticle } from "./personality/PersonalityArticle";
 import { PersonalityLink } from "./personality/PersonalityLink";
 
-export { PersonalityArticle, PersonalityLink };
+export { PersonalityListContainer, PersonalityArticle, PersonalityLink };
