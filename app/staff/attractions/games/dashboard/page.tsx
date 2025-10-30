3dddc487ffe1d71535929fdd4b424715310e4067
yonep3904 <s59201@ga.ariake-nct.ac.jp>
2025-10-30 23:49:00 +0900

    各アトラクションのダッシュボードページの適用

---
+++ app/staff/attractions/games/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { GameDashboardContainer } from "@/components/staff/dashboard/GameDashboardContainer";
+
+export default function Page() {
+  return (
+    <main className="bg-gray-50">
+      <GameDashboardContainer />
+    </main>
+  );
+}
---
+++ app/staff/attractions/prize/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { EntryDashboardContainer } from "@/components/staff/";
+
+export default function Page() {
+  return (
+    <main className="bg-gray-50">
+      <EntryDashboardContainer attraction="prize" />
+    </main>
+  );
+}
